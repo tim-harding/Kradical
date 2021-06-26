9a04f408be970b39d8ae71b48c1930d49297b4a8
Tim Harding <Tim@TimHarding.co>
2021-06-25 21:50:54 -0700

    Cleaned up parser results

--- src/bin.rs
+++ src/bin.rs
@@ -1,46 +1,43 @@
 extern crate kanji_api;
 
 use anyhow::Result;
-use nom::{IResult, bytes::complete::{take, take_until}, combinator::{eof}, dbg_dmp, multi::{many_till}};
+use nom::{
+    bytes::complete::{take, take_until},
+    combinator::eof,
+    multi::many_till,
+    sequence::pair,
+    IResult,
+};
 use std::fs;
 
 const NEWLINE: &[u8] = &[0x0A];
 
 fn main() -> Result<()> {
     match fs::read("./dictionary-files/downloads/kradfile2") {
-        Ok(text) => {
-            match lines(&text) {
-                Ok(parsed) => {
-                    let (_, results) = parsed;
-                    println!("{:?}", results.0.len());
-                }
-                Err(err) => match err {
-                    nom::Err::Incomplete(needed) => println!("Incomplete: {:?}", needed),
-                    nom::Err::Error(err) => println!("Error: {:?}", err.input.len()),
-                    nom::Err::Failure(err) => println!("Failure: {:?}", err.code),
-                },
+        Ok(text) => match lines(&text) {
+            Ok(parsed) => {
+                let (_, results) = parsed;
+                println!("{:?}", results.len());
             }
-            let mut count = 0;
-            for byte in text {
-                if byte == 0x0A {
-                    count += 1
-                }
-            }
-            println!("{}", count)
-        }
+            Err(err) => match err {
+                nom::Err::Incomplete(needed) => println!("Incomplete: {:?}", needed),
+                nom::Err::Error(err) => println!("Error: {:?}", err.input.len()),
+                nom::Err::Failure(err) => println!("Failure: {:?}", err.code),
+            },
+        },
         Err(err) => println!("{}", err),
     }
     Ok(())
 }
 
-fn lines(b: &[u8]) -> IResult<&[u8], (Vec<&[u8]>, &[u8])> {
-    many_till(line, eof)(b)
+fn lines(b: &[u8]) -> IResult<&[u8], Vec<&[u8]>> {
+    let (i, o) = many_till(line, eof)(b)?;
+    let (lines, _end) = o;
+    Ok((i, lines))
 }
 
 fn line(b: &[u8]) -> IResult<&[u8], &[u8]> {
-    let line = dbg_dmp(take_until(NEWLINE), "line")(b)?;
-    println!("{}, {}", line.0.len(), line.1.len());
-    // println!("{}", String::from_utf8_lossy(line.0));
-    // Ok(line)
-    take(line.1.len() + 1)(b)
+    let (i, o) = pair(take_until(NEWLINE), take(1u8))(b)?;
+    let (line, _newline) = o;
+    Ok((i, line))
 }
