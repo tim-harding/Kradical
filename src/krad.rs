fd0cbba4a0ee61ea5ee4f91e3892793962204b16
Tim Harding <Tim@TimHarding.co>
2021-06-25 22:56:25 -0700

    Parser complete but with errors

--- src/krad.rs
+++ src/krad.rs
@@ -1,30 +1,48 @@
-use nom::{IResult, bytes::complete::{tag, take, take_until}, combinator::eof, multi::many_till, sequence::{pair, separated_pair, terminated}};
+use nom::{IResult, branch::alt, bytes::complete::{tag, take, take_until}, combinator::{eof, map, map_opt, value}, multi::many_till, sequence::{pair, separated_pair, terminated}};
 
 const NEWLINE: &[u8] = &[0x0A];
 const SEPARATOR: &[u8] = &[0x20, 0x3A, 0x20];
 const POUND: &[u8] = &[0x23];
 const SPACE: &[u8] = &[0x20];
 
-pub fn lines(b: &[u8]) -> IResult<&[u8], Vec<&[u8]>> {
+// Todo: Shouldn't need to clone this
+#[derive(Debug, Clone)]
+pub struct KanjiParts<'a> {
+    kanji: &'a[u8],
+    radicals: Vec<&'a[u8]>,
+}
+
+pub fn lines(b: &[u8]) -> IResult<&[u8], Vec<Option<KanjiParts>>> {
     let (i, o) = many_till(line, eof)(b)?;
     let (lines, _end) = o;
     Ok((i, lines))
 }
 
-fn line(b: &[u8]) -> IResult<&[u8], &[u8]> {
-    let (i, o) = pair(take_until(NEWLINE), take(1u8))(b)?;
-    let (line, _newline) = o;
-    Ok((i, line))
+fn line(b: &[u8]) -> IResult<&[u8], Option<KanjiParts>> {
+    alt((value(None, comment), map(kanji_line, |k| Some(k))))(b)
 }
 
-struct KanjiParts<'a> {
-    kanji: &'a[u8],
-    parts: Vec<&'a[u8]>,
+fn kanji_line(b: &[u8]) -> IResult<&[u8], KanjiParts> {
+    let (i, o) = separated_pair(take_until(SEPARATOR), tag(SEPARATOR), radicals)(b)?;
+    let (kanji, radicals) = o;
+    let parts = KanjiParts{
+        kanji,
+        radicals,
+    };
+    Ok((i, parts))
 }
 
-// fn kanji_line(b: &[u8]) -> IResult<&[u8], &[u8]> {
-    // let (i, o) = separated_pair(take_until(SEPARATOR), tag(SEPARATOR), )
-// }
+fn radicals(b: &[u8]) -> IResult<&[u8], Vec<&[u8]>> {
+    let (i, o) = pair(many_till(radical, tag(NEWLINE)), take(1u8))(b)?;
+    let ((radicals, _), _newline) = o;
+    Ok((i, radicals))
+}
+
+fn radical(b: &[u8]) -> IResult<&[u8], &[u8]> {
+    let (i, o) = pair(take_until(SPACE), take(1u8))(b)?;
+    let (radical, _space) = o;
+    Ok((i, radical))
+}
 
 fn comment(b: &[u8]) -> IResult<&[u8], ()> {
     let (i, _o) = pair(tag(POUND), end_of_line)(b)?;
