9c562500ec52c2b440a36060754baf22081068ce
Tim Harding <Tim@TimHarding.co>
2021-06-28 15:10:05 -0700

    CLI options setup

--- kradical_converter/src/main.rs
+++ kradical_converter/src/main.rs
@@ -1,8 +1,42 @@
+use clap::{AppSettings, Clap};
 use kradical_parsing::krad::{parse_file, Decomposition, KradError};
 use std::{fs::File, io::Write};
 
+#[derive(Clap, Clone, PartialEq, Eq, Debug)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    #[clap(arg_enum)]
+    input_format: InputFormat,
+
+    #[clap(arg_enum)]
+    output_format: OutputFormat,
+
+    #[clap(short, long, required = true)]
+    inputs: Vec<String>,
+
+    #[clap(short, long)]
+    output: String,
+}
+
+#[derive(PartialEq, Eq, Clone, Copy, Clap, Debug)]
+enum InputFormat {
+    Radk,
+    Krad,
+}
+
+#[derive(PartialEq, Eq, Clone, Copy, Clap, Debug)]
+enum OutputFormat {
+    Unicode,
+    Rust,
+}
+
+fn main() {
+    let opts = Opts::parse();
+    println!("{:?}", opts);
+}
+
 // Todo: Take files to parse and output location as arguments
-fn main() -> Result<(), KradError> {
+fn krad_stuff() -> Result<(), KradError> {
     let mut decompositions = vec![];
     decompositions.extend(parse_kradfile()?);
     decompositions.extend(parse_kradfile2()?);
