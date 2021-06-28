a9169857368d0ff1b888890467a43f1c792b5855
Tim Harding <Tim@TimHarding.co>
2021-06-28 13:32:42 -0700

    Made JIS212 return chars

--- jis/jis212.py
+++ jis/jis212.py
@@ -9,7 +9,7 @@ import re
 pattern = re.compile(r"<U([\dA-F]{4})> \\x([\dA-F]{2})\\x([\dA-F]{2})")
 with open("./jis/jisx-212.ucm", "r") as input:
     with open("./src/jis212.rs", "w") as output:
-        output.write("pub fn jis212_to_utf8(code: u16) -> Option<&'static str> {\n")
+        output.write("pub fn jis212_to_utf8(code: u16) -> Option<char> {\n")
         output.write("\tmatch code {\n")
 
         text = input.read()
@@ -17,7 +17,7 @@ with open("./jis/jisx-212.ucm", "r") as input:
         for match in matches:
             unicode_raw = match[0]
             jis_raw = "".join(match[1:])
-            variant = f"\t\t0x{jis_raw} => Some(\"\\u{{{unicode_raw}}}\"),\n"
+            variant = f"\t\t0x{jis_raw} => Some('\\u{{{unicode_raw}}}'),\n"
             output.write(variant)
 
         output.write("\t\t_ => None,\n")
